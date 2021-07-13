d28b87f9a6fba14c3d6673a64163568f9f991e7d
Ilja Patrushev <slowtnr@gmail.com>
2021-07-13 15:02:15 +0300

    add tableview datasources for reminderlist and detailreminderlist.

--- Reminders/Controllers/ReminderListViewController.swift
+++ Reminders/Controllers/ReminderListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ReminderListViewController: UITableViewController {
     
+    private var reminderListDataSource: ReminderListDataSource?
+    
     static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
@@ -19,39 +21,16 @@ class ReminderListViewController: UITableViewController {
             let reminder = Reminder.testData[indexPath.row]
             destination.configure(with: reminder)
         }
-        
     }
     
-
-}
-
-extension ReminderListViewController {
-    
-    static let reminderListCellIdentifier = "ReminderListCell"
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell
-        else { fatalError("Unable to dequeue ReminderCell") }
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let reminder = Reminder.testData[indexPath.row]
+        reminderListDataSource = ReminderListDataSource()
+        tableView.dataSource = reminderListDataSource
         
-        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)
-        
-        cell.doneButtonAction = {
-            Reminder.testData[indexPath.row].isComplete.toggle()
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        }
-        
-        return cell
     }
     
 }
+
 
